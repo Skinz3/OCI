8bbd6369f23251d66c1b90e22d1395ae859911a8
Skinz3 <oliviamarius35@gmail.com>
2022-02-07 15:09:25 +0100

    Update

---
+++ www/js/API/Firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAPzdd1ADtyOeL1EUuUbx8jFOwkRTYpa3o",
+    authDomain: "navneed-oci.firebaseapp.com",
+    projectId: "navneed-oci",
+    storageBucket: "navneed-oci.appspot.com",
+    messagingSenderId: "743607401529",
+    appId: "1:743607401529:web:4c18016d65060f2e71a1e8",
+    measurementId: "G-1LEKMXRJRC"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth()
+
