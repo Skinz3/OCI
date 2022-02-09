3398ecbeb271ce6e184ea53b92a599c392e24326
Brahmi Said <said.brahmi@etudiant.univ-rennes1.fr>
2022-02-09 13:03:19 +0100

    Implémentation list

--- www/js/OpenStreetMap.js
+++ www/js/OpenStreetMap.js
@@ -49,7 +49,7 @@ class OpenStreetMap {
 
         var latlng = e.latlng.lat + ',' + e.latlng.lng;
         var greenIcon = new L.Icon({
-            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
             shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
             iconSize: [25, 41],
             iconAnchor: [12, 41],
